2857e0114751737c699e74f128dad36f952c8c12
Chaisy <dashulianorton@gmail.com>
2024-07-11 23:27:19 +0300

    feat: update models, add metadata, fix fields

--- api_project/api_product/models.py
+++ api_project/api_product/models.py
@@ -1,18 +1,23 @@
-import uuid
-from django.utils.timezone import timezone
+from datetime import datetime
 
+from django.utils.timezone import timezone
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator
 from django.db import models
 from api_project.api_authentication.models import Teacher
+from api_project.utilities.validators import phone_validator
 import uuid
 
 
 class Article(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=200, blank=False)
-    content = models.TextField(default='no content', blank=False)
-    source = models.CharField(max_length=200, default='', blank=True)
+    title = models.CharField(max_length=200, null=False, blank=False)
+    content = models.TextField(default='no content', null=False, blank=False)
+    source = models.CharField(max_length=200, default='', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
 
     def __str__(self):
         return self.title
@@ -20,26 +25,38 @@ class Article(models.Model):
 
 class CourseCategory(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=100, blank=False, unique=True)
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
 class Discount(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    percent = models.PositiveIntegerField(blank=False, default=0, validators=[MaxValueValidator(100)])
-    description = models.TextField(blank=False)
+    percent = models.PositiveIntegerField(blank=False, null=True, default=0, validators=[MaxValueValidator(100)])
+    description = models.TextField(blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Discount'
+        verbose_name_plural = 'Discounts'
 
     def __str__(self):
-        return self.percent
+        return f"{self.percent} %"
 
 
 class Review(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    author = models.CharField(max_length=50, blank=False)
-    course_category = models.ForeignKey('CourseCategory', on_delete=models.SET_NULL)
-    content = models.TextField(blank=False)
+    author = models.CharField(max_length=50, blank=False, null=False)
+    course_category = models.ForeignKey('CourseCategory', on_delete=models.SET_NULL, blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
 
     def __str__(self):
         return f"Review by {self.author} on {self.course_category.title}"
@@ -47,17 +64,25 @@ class Review(models.Model):
 
 class FAQCategory(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=100, blank=False, unique=True)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
 class FAQ(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.TextField(blank=False)
-    description = models.TextField(blank=False)
-    faq_category = models.ForeignKey('FAQCategory', on_delete=models.SET_NULL)
+    title = models.TextField(blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    faq_category = models.ForeignKey('FAQCategory', on_delete=models.SET_NULL, blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'FaQ'
+        verbose_name_plural = 'FaQs'
 
     def __str__(self):
         return self.title
@@ -65,39 +90,42 @@ class FAQ(models.Model):
 
 class Application(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100, blank=False)
-    surname = models.CharField(max_length=100, blank=False)
-    phone_number = models.CharField(max_length=13, blank=False)
-    email = models.EmailField(blank=False)
-    start_date = models.DateField(blank=False)
-    course = models.ForeignKey('Course', on_delete=models.SET_NULL)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    surname = models.CharField(max_length=100, blank=False, null=False)
+    phone_number = models.CharField(validators=[phone_validator], max_length=13, blank=False, null=False)
+    email = models.EmailField(blank=False, null=True)
+    start_date = models.DateField(blank=False, null=False, auto_now_add=True, default=datetime.now())
+    course = models.ForeignKey('Course', on_delete=models.SET_NULL, blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Application'
+        verbose_name_plural = 'Applications'
 
     def __str__(self):
         return f"{self.name} {self.surname} — {self.course.title}"
 
     def clean(self):
+        super().clean()
         if self.start_date > timezone.now().date():
             raise ValidationError("Start date cannot be in the future.")
-
-    def save(self, *args, **kwargs):
-        self.full_clean()  # Ensure clean() is called before saving
-        super(Application, self).save(*args, **kwargs)
-
-
 
 
 class Course(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     photo = models.ImageField()
-    title = models.CharField(max_length=100, blank=False)
-    description = models.TextField(blank=True)
-    advantages = models.TextField(blank=True)
-    curriculum = models.TextField(blank=True)
-    study_hours = models.IntegerField(blank=False, default=0)
-    price_for_one = models.PositiveIntegerField()
-    price_in_group = models.PositiveIntegerField()
-    tutors = models.ManyToManyField('Tutor')
-    course_category = models.ForeignKey('CourseCategory', on_delete=models.SET_NULL)
+    title = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    advantages = models.TextField(blank=True, null=True)
+    curriculum = models.TextField(blank=True, null=True)
+    study_hours = models.IntegerField(blank=False, null=False, default=0)
+    price_for_one = models.PositiveIntegerField(blank=False, null=False)
+    price_in_group = models.PositiveIntegerField(blank=False, null=False)
+    tutors = models.ManyToManyField('Teacher', blank=False, null=False)
+    course_category = models.ForeignKey('CourseCategory', on_delete=models.SET_NULL, blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
 
     def __str__(self):
         return self.title
@@ -106,7 +134,7 @@ class Course(models.Model):
 class Certificate(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     file = models.FileField(upload_to='certificates/', null=False, blank=False)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=False, blank=False)
+    teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE, null=False, blank=False)
 
     class Meta:
         verbose_name = 'Certificate'
